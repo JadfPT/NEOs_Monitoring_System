640d07f59ff3e7bd665500584005480e1cf98887
JadfPT <jadf.3407@gmail.com>
2026-01-07 05:31:54 +0000

    Add new SQL view files for catalog and various queries
    - Created Catalogo_Lists.sql to define a new view for catalog lists.
    - Added Varias__views_2.sql for additional varied queries.
    - Introduced Varias_views.sql for another set of varied queries.

---
+++ Queries/Views/Varias__views_2.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:27b8443fae5e25af008e46008db0bebca8411c95a2473e9a5b63bb00097f08a4
+size 1509
---
+++ Queries/Views/Varias_views.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd817719b4bf393a227657fd56c6be6272a2b4b897dfa9ec9958150ce4617646
+size 2389
