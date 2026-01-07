4b23dc765fdeb4464a2211c492cad06e90c77eaf
Alexandre Machado <alexandre.axe0246@gmail.com>
2026-01-07 03:51:14 +0000

    hmmokkk

--- Base de Dados/generated_chunks/04_extras.sql
+++ Base de Dados/generated_chunks/04_extras.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a3c58ff4f137a22ef94ff6d70314a143f6cfb785ed5e74fe783cf678cf8d0ce
-size 21707
+oid sha256:32cbf0eef9cfcbce77a2e19054f4fc80970964cd215d517b87d395c9363f7e23
+size 17238
